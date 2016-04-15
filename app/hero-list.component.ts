c4955097dd154964de0d75c02735c2ee49ce3fab
Ice Warlock <bjornserk@gmail.com>
2016-04-15 15:04:50 +0600

    typoscript hero script

---
+++ app/hero-list.component.ts
@@ -0,0 +1,18 @@
+@Component({
+    selector: 'hero-list',
+    templateUrl: 'app/hero-list.component.html',
+    directives: [HeroDetailComponent],
+    providers: [HeroService]
+})
+export class HeroListComponent implements OnInit{
+    constructor(private _service: HeroService) {}
+
+    heroes: Hero[];
+    selectedHero: Hero;
+
+    ngOnInit() {
+        this.heroes = this._service.getHeroes();
+    }
+
+    selectHero(hero: Hero) { this.selectedHero = hero;}
+}
